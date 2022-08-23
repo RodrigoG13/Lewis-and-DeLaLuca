6b925ddcd02d592dbb7aad38bd86eb4aacc185e9
Rodrigo Trejo <rtrejoa1302@gmail.com>
2022-08-23 12:27:17 -0500

    0.0.0
    Creamos el paquete general y el proyecto en netbeans

---
+++ LewisAndDeLaLuca/src/main/java/genero/lewisanddelaluca/LewisAndDeLaLuca.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package genero.lewisanddelaluca;
+
+/**
+ *
+ * @author quiza rodrigo pero probablemente eidan
+ */
+public class LewisAndDeLaLuca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
